082940f9985dd07a1435746d9e4d24497a7e9be1
Ghano_Atm <ghanoatm95@gmail.com>
2022-09-27 19:47:27 +0200

    Portfolio

---
+++ Portfolio.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+
+document.getElementById("content1").style.animation="load 4s both"
+document.getElementById("content2").style.animation="load 4s both"
+document.getElementById("div_contact").style.animation="load 4s both"
+
+}
